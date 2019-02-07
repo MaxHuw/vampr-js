27da8e76cc08acb257d1aa7dfe552a4726c4db35
MaxHuw <m.huw.graham@gmail.com>
2019-02-07 18:51:00 +0000

    First vampire assignment, stretch partially complete.

--- vampire.js
+++ vampire.js
@@ -10,21 +10,36 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let degreeOfSeparation  = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator){
+      currentVampire = currentVampire.creator;
+      degreeOfSeparation++;
+    }
+
+    return degreeOfSeparation;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal){
+      return true;
+    } else {
+      return false;
+    }
 
   }
 
@@ -37,6 +52,21 @@ class Vampire {
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
 
+    let myLiniage = this;
+    let theirLineage = vampire;
+
+    if (!myLiniage.creator){
+      return myLiniage;
+    } else if (!theirLineage.creator){
+      return theirLineage;
+    } else if ()
+   
+    while (myLiniage !== theirLineage){
+      myLiniage = myLiniage.creator;
+      theirLineage = theirLineage.creator;
+    }
+
+    return myLiniage;
   }
 }
 
